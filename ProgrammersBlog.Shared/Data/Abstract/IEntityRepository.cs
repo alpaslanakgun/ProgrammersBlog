0c6b0f3000eb84d64ab9e929cdbada75e3b09fd8
Alpaslan Akgun <alpaslanakgun@aol.com>
2022-09-28 15:45:24 +0300

    Data Update

---
+++ ProgrammersBlog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate,params Expression<Func<T, object>>[] includeProperties);
+
+
+
+    }
+}
